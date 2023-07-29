0095e75ced36dfefb206bf5428b86cceec9d2ade
saitejadl <125654512+saitejadl@users.noreply.github.com>
2023-07-30 05:29:03 +0530

    Update gitedit.py

--- Travels_Car_Driver_Tracking/gitedit.py
+++ Travels_Car_Driver_Tracking/gitedit.py
@@ -3,21 +3,11 @@ import streamlit as st
 st.title('Try to add text')
 
 def write(text):
-    f = open('edit_file.txt','a')
+    f = open('Travels_Car_Driver_Tracking/edit.txt','a')
     f.write('New input: {}\n'.format(text))
     f.close()
-
-def get_file():
-    f = open('edit_file.txt','r')
-    r = f.read()
-    f.close()
-    return r
-
 text = st.text_input('Text')
 st.button('Add text to file',on_click = write,args = [text])
-
-st.download_button('Download text',get_file(),'text_file.txt')
-
 
 
 
