d3c68853c399d30d44118dcf7bbea00c301f13cd
saitejadl <125654512+saitejadl@users.noreply.github.com>
2023-07-30 05:28:18 +0530

    Update gitedit.py

--- Travels_Car_Driver_Tracking/gitedit.py
+++ Travels_Car_Driver_Tracking/gitedit.py
@@ -1,8 +1,31 @@
-from github import Github
-from github import Auth
 import streamlit as st
 
-st.write(st.experimental_user)
+st.title('Try to add text')
+
+def write(text):
+    f = open('edit_file.txt','a')
+    f.write('New input: {}\n'.format(text))
+    f.close()
+
+def get_file():
+    f = open('edit_file.txt','r')
+    r = f.read()
+    f.close()
+    return r
+
+text = st.text_input('Text')
+st.button('Add text to file',on_click = write,args = [text])
+
+st.download_button('Download text',get_file(),'text_file.txt')
+
+
+
+
+# from github import Github
+# from github import Auth
+# import streamlit as st
+
+# st.write(st.experimental_user)
 
 # GITHUB_REPO = 'Travels_Car_Driver_Tracking'
 # GITHUB_TOKEN = st.secrets["GITHUB_TOKEN"]
