bdc069a1aa607fa0589471cabe6d4f211a23df5b
saitejadl <125654512+saitejadl@users.noreply.github.com>
2023-07-30 03:18:42 +0530

    Add files via upload

---
+++ Travels_Car_Driver_Tracking/Admin.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+
+st.subheader("Balaji Travels Admin")
+with open("/content/Users_Password.txt",'r') as f:
+  a = f.readline()[:-2].split(",")
+keys=[]
+vals=[]
+for i in a:
+  keys.append(i.split(":")[0])
+  vals.append(i.split(":")[1])
+u_p = dict(zip(keys,vals))
+
+user_name = st.sidebar.text_input('Username', value="")
+password = st.sidebar.text_input('Password', value="", type="password")
+
+if user_name in u_p.keys():
+  if u_p[user_name]==password:
+    st.sidebar.success("SIGN IN")
+    df = pd.read_csv("/content/Data.txt",sep="|")
+    st.dataframe(data=df,use_container_width=True)
+    c1,c2 = st.columns(2)
+    with c1:
+      col = st.selectbox("Filter by:", df.columns, help='Car Peru')
+    with c2:
+      search = st.selectbox("Filter by:", df[col].unique(), help='Car Peru')
+    st.dataframe(data=df[df[col]==search],use_container_width=True)
+  else:
+    st.sidebar.error("Invalid Password")
+else:
+    st.sidebar.error("Invalid Username")
+
+if st.sidebar.button("SIGN UP"):
+  r_user = st.sidebar.text_input('Username', value="")
+  r_password = st.sidebar.text_input('Password', value="", type="password")
+  r_verify = st.sidebar.text_input('Re-Enter Password', value="", type="password")
+  if(r_password==r_verify):
+    Referal_Code = st.sidebar.text_input('Referal Code', value="")
+    if Referal_Code=="saiteja":
+      with open("/content/Users_Password.txt",'a') as d:
+        d.writelines(f",{r_user}:{r_password}")
+
+
+
+
+
+  
+
---
+++ Travels_Car_Driver_Tracking/User.py
@@ -0,0 +1,23 @@
+import streamlit as st
+st.subheader("Balaji Travels Car Tracking")
+Name = st.text_input("Name of Driver", value=" ", help="Driver Peru", placeholder=None)
+Ph_Num = st.text_input("Driver Phone Number", value=" ", help="Diver Phone Number", placeholder=None)
+c1,c2 = st.columns(2)
+with c1:
+  Car_Name = st.selectbox("Car Name", ['baleeno','ertiga','i10','ferare','Bugati','roles Royes'], help='Car Peru')
+with c2:
+  Car_Num = st.text_input("Car Number", value=" ", help="Car Number Plate", placeholder=None)
+Route = st.text_input("Travel Route", value=" ", help="Car Velle Route", placeholder=None)
+co1,co2,co3 = st.columns(3)
+with co1:
+  Loc = st.text_input("Location", value=" ", help="Car Unna Place", placeholder=None)
+with co2:
+  Date = st.date_input("Date", value=None,help='Date')
+with co3:
+  Time = st.time_input("Time", value=None,help="Time")
+if st.button("Submit"):
+  with open("/content/Data.txt",'a') as f:
+    L = f'\n{Name}|{Ph_Num}|{Car_Name}|{Car_Num}|{Route}|{Loc}|{str(Date)}|{str(Time)}'
+    f.writelines(L)
+    f.close()
+    st.success("Saved")
