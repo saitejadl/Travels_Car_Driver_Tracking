0981034c110de9e7d71c1c477aaca39632344b69
saitejadl <125654512+saitejadl@users.noreply.github.com>
2023-07-30 11:09:42 +0530

    Update User.py

--- Travels_Car_Driver_Tracking/User.py
+++ Travels_Car_Driver_Tracking/User.py
@@ -3,92 +3,43 @@ from github import Auth
 import streamlit as st
 
 st.write(st.experimental_user)
-
 GITHUB_REPO = 'Travels_Car_Driver_Tracking'
 GITHUB_TOKEN = st.secrets["Git_Hub_Token"]
-
 auth = Auth.Token(GITHUB_TOKEN)
 g = Github(auth=auth)
 
-st.title('Try to add text to GitHub')
-
+def get_file():
+    repo = g.get_user().get_repo(GITHUB_REPO)
+    repo_file = repo.get_contents('Travels_Car_Driver_Tracking/Data.txt')
+    file_text = repo_file.decoded_content.decode()
+    return repo, repo_file, file_text
 def write(text):
     r, rf,ft = get_file()
     t = ft + text
     r.update_file(rf.path,'streamlit commit',t,rf.sha,branch='main')
-    
 
-def get_file():
-    repo = g.get_user().get_repo(GITHUB_REPO)
-    repo_file = repo.get_contents('Travels_Car_Driver_Tracking/edit.txt')
-    file_text = repo_file.decoded_content.decode()
-    return repo, repo_file, file_text
-
-text = st.text_input('Text')
+st.subheader("Balaji Travels Car Tracking")
+Driver_Name = st.text_input("Name of Driver", value=" ", help="Driver Name", placeholder=None)
+Driver_Phone_Num = st.text_input("Driver Phone Number", value=" ", help="Diver Phone Number", placeholder=None)
+c1,c2 = st.columns(2)
+with c1:
+  Car_Name = st.selectbox("Car Name", ['baleeno','ertiga','i10','ferare','Bugati','roles Royes'], help='Car Name')
+with c2:
+  Car_Num = st.text_input("Car Number", value=" ", help="Car Number Plate", placeholder=None)
+Route = st.text_input("Travel Route", value=" ", help="Car Travelling Route", placeholder=None)
+co1,co2,co3 = st.columns(3)
+with co1:
+  Loc = st.text_input("Location", value=" ", help="Car Exact Location", placeholder=None)
+with co2:
+  Date = st.date_input("Date", value=None,help='Date')
+with co3:
+  Time = st.time_input("Time", value=None,help="Time")
+text = f'\n{Driver_Name}|{Driver_Phone_Num}|{Car_Name}|{Car_Num}|{Route}|{Loc}|{str(Date)}|{str(Time)}'
 st.button('Add text to file',on_click = write,args = [text])
 
-# st.download_button('Download repo_file',get_file()[1],'repo_file.txt')
-st.download_button('Download file_text',get_file()[2],'file_text.txt')
-
-
-# using an access token
-
-# First create a Github instance:
-
-# Public Web Github
-
-# all_files = []
-# contents = repo.get_contents("tests/test_1.txt")
-
-# print(contents)
-# new_text = contents.decoded_content.decode() + '\nAdding stuff'
-
-# repo.update_file(contents.path,'Adding stuff',new_text,contents.sha,branch='main')
-
-
-
-# from github import Github
-# from github import Auth
-# import streamlit as st
-
-# st.write(st.experimental_user)
-# GITHUB_REPO = 'Travels_Car_Driver_Tracking'
-# GITHUB_TOKEN = st.secrets["Git_Hub_Token"]
-# auth = Auth.Token(GITHUB_TOKEN)
-# g = Github(auth=auth)
-
-# def get_file():
-#     repo = g.get_user().get_repo(GITHUB_REPO)
-#     repo_file = repo.get_contents('Travels_Car_Driver_Tracking/Data.txt')
-#     file_text = repo_file.decoded_content.decode()
-#     return repo, repo_file, file_text
-# def write(text):
-#     r, rf,ft = get_file()
-#     t = ft + text
-#     r.update_file(rf.path,'streamlit commit',t,rf.sha,branch='main')
-
-# st.subheader("Balaji Travels Car Tracking")
-# Driver_Name = st.text_input("Name of Driver", value=" ", help="Driver Name", placeholder=None)
-# Driver_Phone_Num = st.text_input("Driver Phone Number", value=" ", help="Diver Phone Number", placeholder=None)
-# c1,c2 = st.columns(2)
-# with c1:
-#   Car_Name = st.selectbox("Car Name", ['baleeno','ertiga','i10','ferare','Bugati','roles Royes'], help='Car Name')
-# with c2:
-#   Car_Num = st.text_input("Car Number", value=" ", help="Car Number Plate", placeholder=None)
-# Route = st.text_input("Travel Route", value=" ", help="Car Travelling Route", placeholder=None)
-# co1,co2,co3 = st.columns(3)
-# with co1:
-#   Loc = st.text_input("Location", value=" ", help="Car Exact Location", placeholder=None)
-# with co2:
-#   Date = st.date_input("Date", value=None,help='Date')
-# with co3:
-#   Time = st.time_input("Time", value=None,help="Time")
-# text = f'\n{Driver_Name}|{Driver_Phone_Num}|{Car_Name}|{Car_Num}|{Route}|{Loc}|{str(Date)}|{str(Time)}'
-# st.button('Add text to file',on_click = write,args = [text])
-
-# # if st.button("Submit"):
-# #   with open("Travels_Car_Driver_Tracking/Data.txt",'a') as f:
-# #     L = f'\n{Name}|{Ph_Num}|{Car_Name}|{Car_Num}|{Route}|{Loc}|{str(Date)}|{str(Time)}'
-# #     f.writelines(L)
-# #     f.close()
-# #     st.success("Saved")
+# if st.button("Submit"):
+#   with open("Travels_Car_Driver_Tracking/Data.txt",'a') as f:
+#     L = f'\n{Name}|{Ph_Num}|{Car_Name}|{Car_Num}|{Route}|{Loc}|{str(Date)}|{str(Time)}'
+#     f.writelines(L)
+#     f.close()
+#     st.success("Saved")
