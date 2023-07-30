7e8d9882562d0a5780ab83a217ae2109e8ed3be7
saitejadl <125654512+saitejadl@users.noreply.github.com>
2023-07-30 10:27:55 +0530

    Update User.py

--- Travels_Car_Driver_Tracking/User.py
+++ Travels_Car_Driver_Tracking/User.py
@@ -1,4 +1,14 @@
 import streamlit as st
+from github import Github
+from github import Auth
+GITHUB_REPO = 'Travels_Car_Driver_Tracking'
+GITHUB_TOKEN = st.secrets["Git_Hub_Token"]
+auth = Auth.Token(GITHUB_TOKEN)
+g = Github(auth=auth)
+def write(text):
+    r, rf,ft = get_file()
+    t = ft + text
+    r.update_file(rf.path,'streamlit commit',t,rf.sha,branch='main')
 st.subheader("Balaji Travels Car Tracking")
 Name = st.text_input("Name of Driver", value=" ", help="Driver Name", placeholder=None)
 Ph_Num = st.text_input("Driver Phone Number", value=" ", help="Diver Phone Number", placeholder=None)
@@ -15,9 +25,12 @@ with co2:
   Date = st.date_input("Date", value=None,help='Date')
 with co3:
   Time = st.time_input("Time", value=None,help="Time")
-if st.button("Submit"):
-  with open("Travels_Car_Driver_Tracking/Data.txt",'a') as f:
-    L = f'\n{Name}|{Ph_Num}|{Car_Name}|{Car_Num}|{Route}|{Loc}|{str(Date)}|{str(Time)}'
-    f.writelines(L)
-    f.close()
-    st.success("Saved")
+L = f'\n{Name}|{Ph_Num}|{Car_Name}|{Car_Num}|{Route}|{Loc}|{str(Date)}|{str(Time)}'
+st.button('Submit',on_click = write,args = [L])
+
+# if st.button("Submit"):
+#   with open("Travels_Car_Driver_Tracking/Data.txt",'a') as f:
+#     L = f'\n{Name}|{Ph_Num}|{Car_Name}|{Car_Num}|{Route}|{Loc}|{str(Date)}|{str(Time)}'
+#     f.writelines(L)
+#     f.close()
+#     st.success("Saved")
