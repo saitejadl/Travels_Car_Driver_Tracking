12f1a576a97ae90850f497c4911b7d1917b2f6ce
saitejadl <125654512+saitejadl@users.noreply.github.com>
2023-08-06 21:36:05 +0530

    Update User.py

--- Travels_Car_Driver_Tracking/User.py
+++ Travels_Car_Driver_Tracking/User.py
@@ -11,15 +11,14 @@ g = Github(auth=auth)
 def get_file():
     """
     Parameters: None,
-    return    :repo, repo_file, file_textrepository,
-               file in the repository we mention in get_contents() method,
-               Text in the file
+    return    :repo, repo_file, file_text
     Reads the file in repository returns repository, file in the repository we mention in get_contents() method, text in the file
     """
     repo = g.get_user().get_repo(GITHUB_REPO)
     repo_file = repo.get_contents('Travels_Car_Driver_Tracking/Data.txt')
     file_text = repo_file.decoded_content.decode()
     return repo, repo_file, file_text
+    
 def write(text):
     """
     Parameter : text
@@ -31,21 +30,26 @@ def write(text):
     r.update_file(rf.path,'streamlit commit',t,rf.sha,branch='main')
 
 st.subheader("Balaji Travels Car Tracking")
-Driver_Name = st.text_input("Name of Driver", value=" ", help="Driver Name", placeholder=None)
-Driver_Phone_Num = st.text_input("Driver Phone Number", value=" ", help="Diver Phone Number", placeholder=None)
+
+Driver_Name = st.text_input("Name of Driver", value=" ", help="Driver Name", placeholder=None)                                #Driver_Name
+Driver_Phone_Num = st.text_input("Driver Phone Number", value=" ", help="Diver Phone Number", placeholder=None)               #Driver_Phone_Num
+
 c1,c2 = st.columns(2)
 with c1:
-  Car_Name = st.selectbox("Car Name", ['baleeno','ertiga','i10','ferare','Bugati','roles Royes'], help='Car Name')
+  Car_Name = st.selectbox("Car Name", ['baleeno','ertiga','i10','ferare','Bugati','roles Royes'], help='Car Name')            #Car_Name
 with c2:
-  Car_Num = st.text_input("Car Number", value=" ", help="Car Number Plate", placeholder=None)
-Route = st.text_input("Travel Route", value=" ", help="Car Travelling Route", placeholder=None)
+  Car_Num = st.text_input("Car Number", value=" ", help="Car Number Plate", placeholder=None)                                 #Car_Num
+
+Route = st.text_input("Travel Route", value=" ", help="Car Travelling Route", placeholder=None)                               #Route
+
 co1,co2,co3 = st.columns(3)
 with co1:
-  Loc = st.text_input("Location", value=" ", help="Car Exact Location", placeholder=None)
+  Loc = st.text_input("Location", value=" ", help="Car Exact Location", placeholder=None)                                     #Loc
 with co2:
-  Date = st.date_input("Date", value=None,help='Date')
+  Date = st.date_input("Date", value=None,help='Date')                                                                        #Date
 with co3:
-  Time = st.time_input("Time", value=None,help="Time")
+  Time = st.time_input("Time", value=None,help="Time")                                                                        #Time
+    
 text = f'\n{Driver_Name}|{Driver_Phone_Num}|{Car_Name}|{Car_Num}|{Route}|{Loc}|{str(Date)}|{str(Time)}'
 st.button('SUBMIT',on_click = write,args = [text])
 
