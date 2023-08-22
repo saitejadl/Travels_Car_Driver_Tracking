19d7674f7b15ce6bf2100020334c6f6f5ec72ccc
saitejadl <125654512+saitejadl@users.noreply.github.com>
2023-08-22 14:43:27 +0530

    Update Admin.py

--- Travels_Car_Driver_Tracking/Admin.py
+++ Travels_Car_Driver_Tracking/Admin.py
@@ -7,9 +7,12 @@ import json
 import requests
 from streamlit_lottie import st_lottie
 
+#-------Page Congigurations
 PAGE_NAME = "Admin Balaji Travels Tracking | APP"
 PAGE_ICON = "🗺️📌"
 st.set_page_config(page_title=PAGE_NAME, page_icon=PAGE_ICON,layout="wide")
+
+#-------Animation setup
 url1 = requests.get("https://lottie.host/67db9e87-0d87-4959-84b5-a33e9e73d39e/GF5O7NQSHU.json")
 if url1.status_code == 200:
     url_json1 = url1.json()
@@ -43,8 +46,9 @@ def write(text):
     t = ft + text
     r.update_file(rf.path,'streamlit commit',t,rf.sha,branch='main')
 
-
 st.subheader("Balaji Travels Admin")
+
+# Reading Users_Password.txt from github in read mode and converting the read text into u_p dictionary for usernames and passwords
 with open("Travels_Car_Driver_Tracking/Users_Password.txt",'r') as f:
   a = f.read().split("\n")[:-1]
 keys=[]
@@ -57,26 +61,31 @@ u_p = dict(zip(keys,vals))
 st.sidebar.subheader(':blue[SIGNIN]', anchor='signin')
 user_name = st.sidebar.text_input('Username', value="")
 password = st.sidebar.text_input('Password', value="", type="password")
-# st.write(u_p[user_name])
 
+# Authenticating user from u_p dictionary which is created from Users_Password.txt file in gihub
 if user_name in u_p.keys():
   if password==u_p[user_name]:
       
     st.sidebar.success("SIGN IN")
+      
+    # Reading Data.txt from github into a Dataframe by | as a separator
     df = pd.read_csv("Travels_Car_Driver_Tracking/Data.txt",sep="|")
     st.dataframe(data=df,use_container_width=True)
+      
+    # Adding filter mechanism through variables of dataframe then values of that variable
     c1,c2 = st.columns(2)
     with c1:
       col = st.selectbox("Filter by:", df.columns)
     with c2:
       search = st.selectbox("Filter by Value:", df[col].unique())
     st.dataframe(data=df[df[col]==search],use_container_width=True)
+      
   elif password!='':
-    st.sidebar.error("Invalid Password")
+    st.sidebar.error("Invalid Password")      
 elif user_name!='':
     st.sidebar.error("Invalid Username")
     
-for i in range(25):
+for i in range(20):
     st.sidebar.write("###")
     if i==7:
         col1,col2,col3 = st.sidebar.columns([1,6,1])
@@ -99,5 +108,7 @@ with st.sidebar.expander("SIGNUP"):
         st.sidebar.button('SIGNUP',on_click = write,args = [f'{r_user}:{r_password}'])
       elif Referal_Code!='':
         st.sidebar.error("Invalid Referal")
-    
-
+          
+st.write("---")
+st.header("Contact:")
+st.write("+91 9542593585    |        lakshmisaitejadharmada@gmail.com")
